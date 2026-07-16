c1d9d818b06ca9df6a0f20e7544a78a855ea88a8
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-16 20:17:35 +0300

    Add Course Wishes solution

--- A_Course_Wishes.py
+++ A_Course_Wishes.py
@@ -1,51 +1,23 @@
-from collections import deque
-
 t = int(input())
 
 for _ in range(t):
     n, k = map(int, input().split())
-    a = list(map(int, input().split()))
+
+    a = [0] + list(map(int, input().split()))
+
+    courses = []
     b = list(map(int, input().split()))
+    for i in range(n):
+        courses.append((b[i], i + 1))
 
-    levels = [deque() for _ in range(k + 2)]
-    cnt = [0] * (k + 2)
-
-    for i, x in enumerate(b, 1):
-        levels[x].append(i)
-        if x <= k:
-            cnt[x] += 1
+    courses.sort(reverse=True)
 
     ans = []
-    ok = True
 
-    for lv in range(1, k + 1):
-        while cnt[lv]:
-            moved = False
-            for nxt in range(lv + 1, k + 2):
-                if nxt <= k and cnt[nxt] == a[nxt - 1]:
-                    continue
+    for level, idx in courses:
+        while level <= k:
+            ans.append(idx)
+            level += 1
 
-                idx = levels[lv].pop()
-                cnt[lv] -= 1
-                levels[nxt].append(idx)
-                if nxt <= k:
-                    cnt[nxt] += 1
-                ans.append(idx)
-                moved = True
-                break
-
-            if not moved:
-                ok = False
-                break
-
-        if not ok:
-            break
-
-    if not ok or len(ans) > 1000:
-        print(-1)
-    else:
-        print(len(ans))
-        if ans:
-            print(*ans)
-        else:
-            print()
+    print(len(ans))
+    print(*ans)
