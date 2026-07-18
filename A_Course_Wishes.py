5421bf32582740094a4126d5bff77fadeea548ef
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-18 07:39:18 +0300

    Add Codeforces solutions

--- A_Course_Wishes.py
+++ A_Course_Wishes.py
@@ -1,23 +1,42 @@
+from collections import deque
+
 t = int(input())
 
 for _ in range(t):
     n, k = map(int, input().split())
-
-    a = [0] + list(map(int, input().split()))
-
-    courses = []
+    a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    for i in range(n):
-        courses.append((b[i], i + 1))
 
-    courses.sort(reverse=True)
+    pos = [deque() for _ in range(k + 2)]
+    cnt = [0] * (k + 2)
+
+    for i in range(n):
+        pos[b[i]].append(i + 1)
+        cnt[b[i]] += 1
 
     ans = []
 
-    for level, idx in courses:
-        while level <= k:
-            ans.append(idx)
-            level += 1
+    ok = True
 
-    print(len(ans))
-    print(*ans)
+    for level in range(k, 0, -1):
+        while cnt[level] > a[level - 1]:
+            if not pos[level]:
+                ok = False
+                break
+
+            course = pos[level].popleft()
+            ans.append(course)
+
+            cnt[level] -= 1
+            cnt[level + 1] += 1
+            pos[level + 1].append(course)
+
+        if not ok:
+            break
+
+    if not ok:
+        print(-1)
+    else:
+        ans.reverse()
+        print(len(ans))
+        print(*ans)
