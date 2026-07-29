befe0e022e30cdc010cc227b174d1b420714edc6
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-29 10:00:13 +0300

    Add solution for Codeforces 1709B - Also Try Minecraft

--- B_Also_Try_Minecraft.py
+++ B_Also_Try_Minecraft.py
@@ -3,18 +3,20 @@ a = list(map(int, input().split()))
 
 forward = [0] * n
 for i in range(1, n):
-    forward[i] = forward[i - 1] + max(0, a[i - 1] - a[i])
+    forward[i] = forward[i - 1]
+    if a[i] < a[i - 1]:
+        forward[i] += a[i - 1] - a[i]
 
 backward = [0] * n
 for i in range(n - 2, -1, -1):
-    backward[i] = backward[i + 1] + max(0, a[i + 1] - a[i])
+    backward[i] = backward[i + 1]
+    if a[i] < a[i + 1]:
+        backward[i] += a[i + 1] - a[i]
 
 for _ in range(m):
     s, t = map(int, input().split())
-    s -= 1
-    t -= 1
 
     if s < t:
-        print(forward[t] - forward[s])
+        print(forward[t - 1] - forward[s - 1])
     else:
-        print(backward[t] - backward[s])
+        print(backward[t - 1] - backward[s - 1])
